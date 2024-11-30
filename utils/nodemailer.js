104507c26abe03aa74d59327fdc135193e32ba7d
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-11-30 17:03:18 +0530

    nodemailer

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,26 +1,46 @@
 import nodemailer from 'nodemailer'
+import handlebars from 'handlebars';
+import fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
+import { fileURLToPath } from 'url';
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.email,
-        pass: process.env.password
-    },
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-sendEmail = async (to, subject, text) => {
-    try {
-        await transporter.sendMail({
-            from: process.env.email,
-            to,
-            subject,
-            text,
-        });
-        console.log(`Email sent to ${to}`);
-    } catch (error) {
-        console.error('Failed to send email:', error.message);
-        throw error;
-    }
+const loadTemplate = (email, data) => {
+    const filePath = path.join(__dirname, '../View/email', `${email}.html`);
+    const source = fs.readFileSync(filePath, 'utf8');
+    const template = handlebars.compile(source);
+    return template(data);
 };
 
-export default sendEmail;
+const sendmail = async (data) => {
+    const htmlContent = loadTemplate('email', data);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.email,
+            pass: process.env.password
+        },
+    });
+
+    const mailOptions = {
+        from: process.env.email,
+        to,
+        subject,
+        text,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            // logger.error(error);
+        } else {
+            logger.info('Email sent: ' + info.response);
+        }
+    });
+};
+
+export default sendmail;
