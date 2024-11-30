d3f26800fe843a7c076e6ce30d8fd86c39509e90
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-11-30 17:23:11 +0530

    cors update

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app = express()
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://email-flowchart-frontend.vercel.app/",
     credentials: true
 }));
 
